701106ebef1cbb2eb2faa3763a953ec075fe19ed
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-07 19:09:07 +0000

    chore: github login

--- build/esbuild-build.ts
+++ build/esbuild-build.ts
@@ -1,4 +1,6 @@
 import esbuild, { BuildOptions } from "esbuild";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const ENTRY_POINTS = {
   typescript: ["static/main.ts"],
@@ -14,6 +16,10 @@ export const esbuildOptions: BuildOptions = {
   minify: false,
   loader: Object.fromEntries(DATA_URL_LOADERS.map((ext) => [ext, "dataurl"])),
   outdir: "static/dist",
+  define: createEnvDefines(["SUPABASE_URL", "SUPABASE_ANON_KEY"], {
+    SUPABASE_STORAGE_KEY: generateSupabaseStorageKey(),
+    NODE_ENV: process.env.NODE_ENV || "development",
+  }),
 };
 
 async function runBuild() {
@@ -27,3 +33,45 @@ async function runBuild() {
 }
 
 void runBuild();
+
+
+function createEnvDefines(environmentVariables: string[], generatedAtBuild: Record<string, unknown>): Record<string, string> {
+  const defines: Record<string, string> = {};
+  for (const name of environmentVariables) {
+    const envVar = process.env[name];
+    if (envVar !== undefined) {
+      defines[name] = JSON.stringify(envVar);
+    } else {
+      throw new Error(`Missing environment variable: ${name}`);
+    }
+  }
+  for (const key in generatedAtBuild) {
+    if (Object.prototype.hasOwnProperty.call(generatedAtBuild, key)) {
+      defines[key] = JSON.stringify(generatedAtBuild[key]);
+    }
+  }
+  return defines;
+}
+
+export function generateSupabaseStorageKey(): string | null {
+  const SUPABASE_URL = process.env.SUPABASE_URL;
+  if (!SUPABASE_URL) {
+    console.error("SUPABASE_URL environment variable is not set");
+    return null;
+  }
+
+  const urlParts = SUPABASE_URL.split(".");
+  if (urlParts.length === 0) {
+    console.error("Invalid SUPABASE_URL environment variable");
+    return null;
+  }
+
+  const domain = urlParts[0];
+  const lastSlashIndex = domain.lastIndexOf("/");
+  if (lastSlashIndex === -1) {
+    console.error("Invalid SUPABASE_URL format");
+    return null;
+  }
+
+  return domain.substring(lastSlashIndex + 1);
+}
--- static/main.ts
+++ static/main.ts
@@ -1,7 +1,18 @@
+import { AuthService } from "./scripts/authentication";
 import { ManifestDecoder } from "./scripts/decode-manifest";
 import { ManifestRenderer } from "./scripts/render-manifest";
 
+async function handleAuth() {
+  const auth = new AuthService();
+  await auth.renderGithubLoginButton();
+  const token = await auth.getGitHubAccessToken();
+  if (!token) {
+    // await auth.signInWithGithub(); force a login?
+  }
+}
+
 export async function mainModule() {
+  await handleAuth();
   try {
     const decoder = new ManifestDecoder(window.location.search.substring(1));
     const decodedManifest = await decoder.decodeManifest();
