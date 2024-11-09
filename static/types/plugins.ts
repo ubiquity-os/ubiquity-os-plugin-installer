0dbe64cb9ed5e4715f28eb1886f6d750bec78a8b
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-09 05:38:33 +0000

    chore: update types, remove table header

--- static/types/plugins.ts
+++ static/types/plugins.ts
@@ -1,43 +1,45 @@
 export type PluginConfig = {
-    plugins: Plugin[];
+  plugins: Plugin[];
 };
 
 export interface Plugin {
-    uses: Uses[];
+  uses: Uses[];
 }
 
 export interface Uses {
-    plugin: string;
-    with: With;
+  plugin: string;
+  with: With;
 }
 
-export interface With extends Record<string, unknown> { }
+export interface With extends Record<string, unknown> {}
 
 export interface ManifestPreDecode extends Manifest {
-    actionUrl?: string;
-    workerUrl?: string;
-    error?: string;
+  actionUrl?: string;
+  workerUrl?: string;
+  error?: string;
 }
 
 export type ManifestCache = Record<string, ManifestPreDecode>;
 
 export type Manifest = {
-    name: string;
-    description: string;
-    "ubiquity:listeners": string[];
-    commands?: {
-        [key: string]: {
-            example: string;
-            description: string;
-        };
+  name: string;
+  description: string;
+  "ubiquity:listeners": string[];
+  commands?: {
+    [key: string]: {
+      example: string;
+      description: string;
     };
-    configuration: {
-        type: string;
-        properties: {
-            [key: string]: {
-                default: unknown;
-                type?: string;
-            };
-        };
+  };
+  configuration: {
+    type: string;
+    properties: {
+      [key: string]: {
+        default: unknown;
+        type?: string;
+      };
     };
+  };
 };
+
+export type ManifestProps = { type: string; default: string; items?: { type: string }; properties?: Record<string, ManifestProps> };
