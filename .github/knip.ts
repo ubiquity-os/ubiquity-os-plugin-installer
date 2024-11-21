8ff9501ade288c8809a7156f1412ce0fa8a88bba
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-21 10:37:39 +0000

    chore: knip

--- .github/knip.ts
+++ .github/knip.ts
@@ -6,7 +6,17 @@ const config: KnipConfig = {
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["@mswjs/data", "@supabase/supabase-js", "@ubiquity-os/ubiquity-os-kernel", "ajv", "yaml", "simple-git", "@actions/core", "esbuild"],
+  ignoreDependencies: [
+    "@mswjs/data",
+    "@supabase/supabase-js",
+    "@ubiquity-os/ubiquity-os-kernel",
+    "ajv",
+    "yaml",
+    "simple-git",
+    "@actions/core",
+    "esbuild",
+    "@ubiquity-os/plugin-sdk",
+  ],
   eslint: true,
 };
 
